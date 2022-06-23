e660312ed7f04903c3ac736fe77d5ca9ade172ce
MatheusLicazali <licazalilkz@gmail.com>
2022-06-23 07:59:06 -0300

    task created and task routes

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -4,7 +4,18 @@ module.exports = class TaskController {
   static CreateTask(_req, res) {
     res.render("tasks/create");
   }
-  static showTasks(_req, res) {
-    res.render("tasks/all");
+  static async CreateTaskSave(req, res) {
+    const task = {
+      title: req.body.title,
+      description: req.body.description,
+      done: false,
+    };
+    await Task.create(task);
+    res.redirect("/tasks");
+  }
+  static async showTasks(_req, res) {
+    const tasks = await Task.findAll({ raw: true });
+
+    res.render("tasks/all", { tasks });
   }
 };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const TaskController = require("../controllers/TaskController");
 
 router.get("/add", TaskController.CreateTask);
+router.post("/add", TaskController.CreateTaskSave);
 router.get("/", TaskController.showTasks);
 
 module.exports = router;
